d5188e3167dba17834d9baca9e005dba5f38e43f
Tasso <53788301+matheustasso@users.noreply.github.com>
2021-03-08 03:09:47 -0300

    Update calc029.c

--- Atividades/AT02/calc029.c
+++ Atividades/AT02/calc029.c
@@ -4,57 +4,50 @@
 int main(void){
 	
     int input;
-    float a, b, c;
-    int continua = 1;
+    float x, y, z;
+    int go = 1;
     
 
-    while(continua != 0){
-	    
+    while(go != 0){
+
         puts("\nSelecione uma das operaçoes para dois num:\n");
-        puts("0 - Exit\n");
-        puts("1 - Soma\n");
-        puts("2 - Subt\n");
-        puts("3 - Multi\n");
-        puts("4 - Div\n");
-    
+        	puts("0 - Exit\n");
+        	puts("1 - Soma\n");
+        	puts("2 - Subt\n");
+        	puts("3 - Multi\n");
+        	puts("4 - Div\n");
+   
         scanf("%d", &input);
 
         if(input == 0){
-    
-	    continua = 0;
+	    go = 0;
             return 0;
-    
         }
     
         puts("\nSelecione dois valores int:\n");
-        scanf("%f", &a);
-        scanf("%f", &b);
+        scanf("%f", &x);
+        scanf("%f", &y);
 
         switch (input){
-
-           // case 0:
-           // return 0;
-           // break;
-
             case 1 :
-            c = a + b;
-            printf ("%.2f", c);
-            break;
+		    z = x + y;
+		    printf ("%.2f", z);
+		    break;
     
             case 2 :
-            c = a - b;
-            printf ("%.2f", c);
-            break;
+		    z = x - y;
+		    printf ("%.2f", z);
+		    break;
     
             case 3 :
-            c = a * b;
-            printf ("%.2f", c);
-            break;
+		    z = x * y;
+		    printf ("%.2f", z);
+		    break;
     
             case 4 :
-            c = a / b;
-            printf ("%.2f", c);
-            break;
+		    z = x / y;
+		    printf ("%.2f", z);
+		    break;
     
             default :
             printf ("Error\n");
