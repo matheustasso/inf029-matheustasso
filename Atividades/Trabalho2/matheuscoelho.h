e6cb6da59650c415680f03cd5adad52e902565ae
Tasso <53788301+matheustasso@users.noreply.github.com>
2021-06-24 11:11:31 -0300

    codigo original

--- Atividades/Trabalho2/matheuscoelho.h
+++ Atividades/Trabalho2/matheuscoelho.h
@@ -1,40 +1,38 @@
-#ifndef TRABALHO2_ESTRUTURAVETORES_H
-#define TRABALHO2_ESTRUTURAVETORES_H
+#define SUCESSO -1
+#define SEM_ESPACO -2
+#define SEM_ESTRUTURA_AUXILIAR -3
+#define JA_TEM_ESTRUTURA_AUXILIAR -4
+#define POSICAO_INVALIDA -5
+#define SEM_ESPACO_DE_MEMORIA -6
+#define TAMANHO_INVALIDO -7
+#define ESTRUTURA_AUXILIAR_VAZIA -8
+#define NUMERO_INEXISTENTE -9
+#define NOVO_TAMANHO_INVALIDO -10
 
-enum { TODAS_ESTRUTURAS_AUXILIARES_VAZIAS = -11, NOVO_TAMANHO_INVALIDO, NUMERO_INEXISTENTE,
-       ESTRUTURA_AUXILIAR_VAZIA, TAMANHO_INVALIDO, SEM_ESPACO_DE_MEMORIA, POSICAO_INVALIDA,
-       JA_TEM_ESTRUTURA_AUXILIAR, SEM_ESTRUTURA_AUXILIAR, SEM_ESPACO, SUCESSO };
+#define TAMANHO 10
 
-// Estrutura Principal
-typedef struct principal{
-	int *auxiliar;
-	int tamanho;
-	int qtd;
-}Principal;
+typedef struct vetorPrincipal {
+    int *auxiliar; //ponteiro para estrutura auxliliar
+    int  tamanho; // tamanho para alocar a estrutura auxiliar
+    int quantidade; //contador de elementos no vetor
+}PRINCIPAL;
 
-typedef struct reg {
-    int conteudo;
-    struct reg *prox;
-} No;
-
-int ehPosicaoValida(int posicao);
-int todasEstruturasAuxiliaresVazias();
-int criarEstruturaAuxiliar(int posicao, int tamanho);
-int inserirNumeroEmEstrutura(int posicao, int valor);
+int criarEstruturaAuxiliar(PRINCIPAL *vetor_principal, int tamanho, int posicao); //
+int inserirNumeroEmEstrutura(PRINCIPAL *vetor_principal, int valor, int posicao); //
 int excluirNumeroDoFinaldaEstrutura(int posicao);
-int excluirNumeroEspecificoDeEstrutura(int posicao, int valor);
-int getDadosEstruturaAuxiliar(int posicao, int vetorAux[]);
-int getDadosOrdenadosEstruturaAuxiliar(int posicao, int vetorAux[]);
-int getDadosDeTodasEstruturasAuxiliares(int vetorAux[]);
-int getDadosOrdenadosDeTodasEstruturasAuxiliares(int vetorAux[]);
-int modificarTamanhoEstruturaAuxiliar(int posicao, int novoTamanho);
-int getQuantidadeElementosEstruturaAuxiliar(int posicao);
-No *montarListaEncadeadaComCabecote();
-void getDadosListaEncadeadaComCabecote(No *inicio, int vetorAux[]);
-void destruirListaEncadeadaComCabecote(No **inicio);
+int excluirNumeroEspecificoDeEstrutura(int valor, int posicao);
+int getDadosEstruturaAuxiliar(PRINCIPAL *vetor_principal, int posicao);
+int getDadosOrdenadosEstruturaAuxiliar(int posicao, int auxiliar[]);
+int getDadosDeTodasEstruturasAuxiliares(PRINCIPAL *vetor_principal, int vetorAux[]);
+int getDadosOrdenadosDeTodasEstruturasAuxiliares(int auxiliar[]);
+int modificarTamanhoEstruturaAuxiliar(PRINCIPAL *vetor_principal, int posicao, int novoTamanho);
+int getQuantidadeElementosEstruturaAuxiliar(PRINCIPAL *vetor_principal, int posicao);
+int ehPosicaoValida(int posicao);
+void inicializar(PRINCIPAL *vetor_principal);
+void finalizar(PRINCIPAL *vetor_principal);
+int verificaEstrutura(PRINCIPAL *vetor_principal, int posicao);
+int menu();
 
-void inicializar();
-void finalizar();
-void dobrar(int *x);
-
-#endif  // TRABALHO2_ESTRUTURAVETORES_H
+/*
+criarEstruturaAuxiliar {vetor[posicao].auxiliar = malloc (tamanho);}
+*/
